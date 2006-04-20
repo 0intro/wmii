f96eb8db99264e53e6692f477a278eeec82fca70
Sander van Dijk <sander@wmii.de>
2006-04-20 23:18:13 +0000

    fixes the wmiirc rerun issue reported by Neptun (I threw away a little too much a couple of patches ago, sorry...)

--- cmd/wm/column.c
+++ cmd/wm/column.c
@@ -300,6 +300,9 @@ drop_moving(Frame *f, XRectangle *new, XPoint *pt)
 	View *v = src->view;
 	unsigned int i;
 
+	if(!pt)
+		return;
+
 	for(i = 1; (i < v->area.size) &&
 			!blitz_ispointinrect(pt->x, pt->y, &v->area.data[i]->rect); i++);
 	if((tgt = ((i < v->area.size) ? v->area.data[i] : nil))) {
