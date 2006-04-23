956b52bb2a2d344fcff5c8c84771781d7b3444c1
Anselm R. Garbe <garbeam@wmii.de>
2006-04-23 20:39:58 +0200

    implemented Xutf8DrawString behavior

--- liblitz/font.c
+++ liblitz/font.c
@@ -5,20 +5,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cext.h>
 
 #include "blitz.h"
 
 void
 blitz_loadfont(Display *dpy, BlitzFont *font, char *fontstr)
 {
+	char *fontname = fontstr;
+	char **missing = nil, *def = "?";
+	int nmissing;
+	if(font->set)
+		XFreeFontSet(dpy, font->set);
 	if(font->font)
 		XFreeFont(dpy, font->font);
-	font->font = XLoadQueryFont(dpy, fontstr);
+	font->font = XLoadQueryFont(dpy, fontname);
 	if (!font->font) {
-		font->font = XLoadQueryFont(dpy, "fixed");
+		fontname = "fixed";
+		font->font = XLoadQueryFont(dpy, fontname);
 		if (!font->font) {
-			fprintf(stderr, "%s", "liblitz: error, cannot load fixed font\n");
+			fprintf(stderr, "%s", "liblitz: error, cannot load 'fixed' font\n");
 			exit(1);
 		}
 	}
+	font->set = XCreateFontSet(dpy, fontname, &missing, &nmissing, &def);
+
+	if(missing)
+		XFreeStringList(missing);
 }
