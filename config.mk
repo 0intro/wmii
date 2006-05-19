4acfe7cbe7b1d17fe7fb8ea34aa82d607bfefa71
Anselm R. Garbe <garbeam@wmii.de>
2006-05-19 13:35:48 +0200

    wmii-4-current development begins, feature unfreeze

--- config.mk
+++ config.mk
@@ -8,15 +8,16 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-VERSION = 3-current
+VERSION = 4-current
 
 # includes and libs
 LIBS = -L${PREFIX}/lib -L/usr/lib -lc -L${X11LIB} -lX11
 
 # Linux/BSD
-CFLAGS = -O2 -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
+CFLAGS = -g -Wall -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
 	-DVERSION=\"${VERSION}\"
-LDFLAGS = ${LIBS}
+LDFLAGS = -g ${LIBS}
+
 
 # Solaris
 #CFLAGS = -fast -xtarget=ultra ${INCLUDES} -DVERSION=\"${VERSION}\"
