afda47cbf9d9e7aedf94856e8571cd1e4c737959
Anselm R. Garbe <garbeam@wmii.de>
2006-06-06 17:35:29 +0200

    restoring wmiirc, using 50/50 instead of 60/40 by default

--- config.mk
+++ config.mk
@@ -8,7 +8,7 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-VERSION = 20060531
+VERSION = 4-current
 
 # includes and libs
 LIBS = -L${PREFIX}/lib -L/usr/lib -lc -L${X11LIB} -lX11
