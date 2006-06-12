59a06d3c5dbf175fbfabad5f5f8e468878ffe488
Kris Maglione <bsdaemon@wmii.de>
2006-06-11 21:56:08 -0400

    Make sure wmiir read /bar/../.. returns with error.

--- cmd/wm/fs.c
+++ cmd/wm/fs.c
@@ -678,8 +678,8 @@ xwalk(IXPConn *c, Fcall *fcall)
 		if(qsel >= IXP_MAX_WELEM)
 			break;
 		if(!strncmp(fcall->wname[nwqid], "..", 3)) {
-			if(qsel)
-				qsel--;
+			if(!qsel--)
+				return Enofile;
 			qid = &wqid[qsel];
 		}
 		else {
