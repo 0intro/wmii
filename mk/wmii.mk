652ecd53c7b0cfdc40d0e724606ceb4e95572268
Kris Maglione <jg@suckless.org>
2008-03-14 09:04:34 -0400

    Fix ticket #17.

--- mk/wmii.mk
+++ mk/wmii.mk
@@ -1,4 +1,5 @@
-VERSION = $$(hg tip --template 'hg{rev}' 2>/dev/null)
-VERSION := $(shell hg tip --template 'hg{rev}' 2>/dev/null)
-VERSION != hg tip --template 'hg{rev}' 2>/dev/null
+VERS = hg$$(hg identify -n)
+VERSION = $(VERS)
+VERSION := $(shell echo $(VERS) 2>/dev/null)
+VERSION != echo $(VERS) 2>/dev/null
 CONFVERSION = 3.5
