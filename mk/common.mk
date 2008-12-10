cd8aeb73f8addc01746657f979616fe9d475bd60
Kris Maglione <jg@suckless.org>
2008-12-10 18:23:26 -0500

    Don't make extra install directories

--- mk/common.mk
+++ mk/common.mk
@@ -4,7 +4,7 @@ install: all
 
 MANDIRS=$(MAN)/man1
 mkdirs:
-	for i in $(BIN) $(ETC) $(LIBDIR) $(MANDIRS) $(INCLUDE) $(DIRS); do \
+	for i in $(BIN) $(ETC) $(LIBDIR) $(MANDIRS) $(INCLUDE); do \
 		test -d $(DESTDIR)$$i || echo MKDIR $$i; \
 		mkdir -pm 0755 $(DESTDIR)$$i; \
 	done
