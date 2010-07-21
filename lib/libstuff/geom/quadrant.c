6868022346288eb52f439d13173d167209bd5a55
Kris Maglione <kris@suckless.org>
2010-07-21 10:08:01 -0400

    Fix quadrant(). Closes issue #210.

--- lib/libstuff/geom/quadrant.c
+++ lib/libstuff/geom/quadrant.c
@@ -8,16 +8,10 @@ quadrant(Rectangle r, Point pt) {
 	Align ret;
 
 	pt = subpt(pt, r.min);
-	ret = 0;
-
-	if(pt.x >= Dx(r) * .5)
-		ret |= East;
-	if(pt.x <= Dx(r) * .5)
-		ret |= West;
-	if(pt.y <= Dy(r) * .5)
-		ret |= North;
-	if(pt.y >= Dy(r) * .5)
-		ret |= South;
+	ret = East  * (pt.x >= Dx(r) * .5)
+	    | West  * (pt.x <  Dx(r) * .5)
+	    | South * (pt.y >= Dy(r) * .5)
+	    | North * (pt.y <  Dy(r) * .5);
 
 	return ret;
 }
