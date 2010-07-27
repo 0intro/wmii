05053cb9954cdfab6c87d07f66ed34d19f9aa30a
Kris Maglione <kris@suckless.org>
2010-07-27 14:45:42 -0400

    Don't allow extra content after colors ctl commands.

--- cmd/wmii/message.c
+++ cmd/wmii/message.c
@@ -906,15 +906,15 @@ msg_nudge(View *v, IxpMsg *m) {
 
 void
 msg_parsecolors(IxpMsg *m, CTuple *col) {
+	CTuple tpl;
 	static char Ebad[] = "bad color string";
 	char n;
 
-	n = loadcolor(col, m->pos, m->end);
-	if(n == 0)
-		error(Ebad);
-
+	n = loadcolor(&tpl, m->pos, m->end);
 	m->pos += n;
-	msg_eatrunes(m, isspacerune, true);
+	if(n == 0 || msg_getword(m, nil))
+		error(Ebad);
+	*col = tpl;
 }
 
 char*
