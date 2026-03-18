4f5702f2f2b3695c32cbac8ef5dd71fb88c0a5de
David du Colombier <0intro@gmail.com>
2026-03-18 21:12:11 +0100

    mk/python.mk: use pip install instead of deprecated setup.py install

--- mk/python.mk
+++ mk/python.mk
@@ -4,10 +4,12 @@ install: $(PYMODULES:%=%.install)
 clean: pyclean
 
 .py.install:
-	echo PYTHON install $* $(PYPREFIX)
+	echo PYTHON install $*
 	DESTDIR=$(DESTDIR); \
-	$(DEBUG) $(PYTHON) $< install -c --root=$${DESTDIR:-/} $(PYPREFIX)
-	rm -rf build $*.egg-info
+	cp -f $*.py setup.py && \
+	$(DEBUG) $(PYTHON) -m pip install --root=$${DESTDIR:-/} $(PYPREFIX) --no-deps --no-build-isolation --ignore-installed . ; \
+	rm -f setup.py
+	rm -rf build $*.egg-info *.dist-info
 pyclean:
 	echo CLEAN build/
 	rm -rf build *.egg-info
