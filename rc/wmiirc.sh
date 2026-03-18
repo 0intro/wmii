ed32573a21c469eab5517ec8f9979e26d5a6a35b
David du Colombier <0intro@gmail.com>
2026-03-18 21:12:11 +0100

    rc/wmiirc.sh: use correct client id in Unresponsive handler
    The handler read /client/sel/label instead of /client/$client/label.
    Since the handler runs in the background, the selected client may
    have changed by the time the dialog appears.

--- rc/wmiirc.sh
+++ rc/wmiirc.sh
@@ -102,7 +102,7 @@ Event Unresponsive
 		msg="The following client is not responding. What would you like to do?$wi_newline"
 		resp=$(wihack -transient $client \
 			      xmessage -nearmouse -buttons Kill,Wait -print \
-			      -fn "${WMII_FONT%%,*}" "$msg $(wmiir read /client/sel/label)")
+			      -fn "${WMII_FONT%%,*}" "$msg $(wmiir read /client/$client/label)")
 		if [ "$resp" = Kill ]; then
 			wmiir xwrite /client/$client/ctl slay &
 		fi
