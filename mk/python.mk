a2107ab39dac93afd2042d5c6a249a1839914c7c
David du Colombier <0intro@gmail.com>
2026-03-18 21:12:11 +0100

    mk/python.mk: remove build artifacts after install to avoid access right error during clean

--- mk/python.mk
+++ mk/python.mk
@@ -7,8 +7,9 @@ clean: pyclean
 	echo PYTHON install $* $(PYPREFIX)
 	DESTDIR=$(DESTDIR); \
 	$(DEBUG) $(PYTHON) $< install -c --root=$${DESTDIR:-/} $(PYPREFIX)
+	rm -rf build $*.egg-info
 pyclean:
 	echo CLEAN build/
-	rm -rf build
+	rm -rf build *.egg-info
 
 .PHONY: pybuild pyclean
